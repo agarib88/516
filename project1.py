024b7712506d825bf5626c4df8179bc79014af80
Ali Garib <59935053+agarib88@users.noreply.github.com>
2021-12-01 08:24:33 -0600

    Update project1.py

--- project1.py
+++ project1.py
@@ -21,11 +21,11 @@ contains_isTitle = [word for word in text6 if word.istitle()] #this condition wo
 print (contains_isTitle) #executes the condition
 
 
-
-FEEDBACK UPDATE:
+Feedback Update:
 import nltk
 from nltk.book import*
-aliList = [word for word in text6 if word.endswith('ise') or 'z' in word or 'pt'in word or word.istitle()] #this condition would list words in text 6 if the words end with 'endings'. 
+aliList = [word for word in text6 if word.endswith('ise') or 'z' in word or 'pt'in word or word.istitle()] #this condition would list 
+#words in text 6 if the words end with 'endings'. 
 print (aliList) #executes the condition
 
 #Exercise 25:
@@ -42,3 +42,18 @@ print (contains_sh) #this prints the words that start with 'sh' in "sent"
 
 word_greaterThan4 = [word for word in sent if len(word)>4] #this variable looks for words whose length is greater than 4 letters in the list of "sent"
 print (word_greaterThan4) #this prints the words that are longer than 4 letters in "sent"
+
+
+Feedback Update:
+  
+  sent = ['she', 'sells', 'sea', 'shells', 'by', 'the', 'sea', 'shore'] #defining the variable "sent" with a list of the given words..
+
+contains_sh = [ ] #this code looks for words that begin with 'sh' in the variable "sent"
+for word in sent :
+    if word.startswith ('sh') and len(word)>4: # these are two contditions together and if this satifies, then add the word to the list.
+        contains_sh.append(word)
+
+print (contains_sh) #this prints the words that start with 'sh' in "sent"
+word_greaterThan4 = [word for word in sent if len(word)>4] #this variable looks for words whose length is greater than 4 letters in the list of "sent"
+print (word_greaterThan4) #this prints the words that are longer than 4 letters in "sent"
+
